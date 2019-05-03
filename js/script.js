3eb12912b09c75843bc9948d4889168c006d3cdd
urza <urza@urza.cc>
2019-05-03 21:28:32 +0200

    czech version of passphrase generator
    words from czech linquistic corpus dictionary, 72000 common words http://ucnk.korpus.cz/retrograd10.php
    solarized colors of html page

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function generatePassword(numberOfWords) {
 
   // Grab a random word, push it to the password array
   for (var i = 0; i < array.length; i++) {
-      var index = (array[i] % 5852);
+      var index = (array[i] % wordlist.length);
       generatedPasswordArray.push(wordlist[index]);
   }
 
@@ -26,61 +26,18 @@ function setStyleFromWordNumber(passwordField, numberOfWords) {
   passwordField.setAttribute('style', 'font-size: ' + newSize + 'px;');
 }
 
-function convertSecondsToReadable(seconds) {
-  var timeString = '';
-  var crackabilityColor = 'green';
-
-  // Enumerate all the numbers
-  var numMilliseconds = seconds * 1000;
-  var numSeconds     = Math.floor(seconds);
-  var numMinutes     = Math.floor(numSeconds / 60);
-  var numHours       = Math.floor(numSeconds / (60 * 60));
-  var numDays        = Math.floor(numSeconds / (60 * 60 * 24));
-  var numYears       = Math.floor(numSeconds / (60 * 60 * 24 * 365));
-  var numCenturies   = Math.floor(numSeconds / (60 * 60 * 24 * 365 * 100));
-
-  if (numMilliseconds < 1000) {
-    timeString = numMilliseconds + ' milliseconds';
-  } else if (numSeconds < 60) {
-    timeString = numSeconds + ' seconds';
-  } else if (numMinutes < 60) {
-    timeString = numMinutes + ' minutes';
-  } else if (numHours < 24) {
-    timeString = numHours + ' hours';
-  } else if (numDays < 365) {
-    timeString = numDays + ' days';
-  } else if (numYears < 100) {
-    timeString = numYears + ' years';
-  } else {
-    timeString = numCenturies + ' centuries';
-  }
-
-  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
-function calculateAndSetCrackTime() {
-  var timeToCrack = zxcvbn(passwordField.value);
-  var readableCrackTime = convertSecondsToReadable(timeToCrack.crack_time);
-  document.querySelector('.crack-time').innerHTML = readableCrackTime;
-}
-
 var selectField = document.getElementById('passphrase_select');
 var passwordField = document.getElementById('passphrase');
 var button = document.querySelector('.btn-generate');
 
 // Initially run it upon load
-passwordField.setAttribute('value', generatePassword(4));
-calculateAndSetCrackTime();
+passwordField.innerHTML = generatePassword(4);
 
 // Listen for a button click
 button.addEventListener('click', function() {
   var numberOfWords = selectField.options[selectField.selectedIndex].value;
-  passwordField.value = generatePassword(numberOfWords);
+  passwordField.innerHTML =  generatePassword(numberOfWords);
   setStyleFromWordNumber(passwordField, numberOfWords);
-  calculateAndSetCrackTime();
 });
 
-// Listen for password value change
-passwordField.addEventListener('input', function (evt) {
-  calculateAndSetCrackTime();
-});
+
