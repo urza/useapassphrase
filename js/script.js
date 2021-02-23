a1926cf183aafac1f92e87db0923da731252b8fd
urza <urza@urza.cc>
2021-02-23 16:46:58 +0100

    Adjust randomization algorithm to remove bias -- manual cherrypick from upstream https://github.com/mike-hearn/useapassphrase/commit/0aa28d4f93c6e760abd4aad972c496bfbddc85c0

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 'use strict';
 
+// Cryptographic replacement for Math.random()
+function randomNumberBetweenZeroAndOne() {
+  var crypto = window.crypto || window.msCrypto;
+  return crypto.getRandomValues(new Uint32Array(1))[0] / 4294967295;
+}
+
 function generatePassword(numberOfWords) {
   // Cryptographically generated random numbers
   numberOfWords = parseInt(numberOfWords);
-  var array = new Uint32Array(numberOfWords);
-  var crypto = window.crypto || window.msCrypto;
-  crypto.getRandomValues(array);
+  // var array = new Uint32Array(numberOfWords);
+  // var crypto = window.crypto || window.msCrypto;
+  // crypto.getRandomValues(array);
 
   // Empty array to be filled with wordlist
   var generatedPasswordArray = [];
 
 
   // Grab a random word, push it to the password array
-  for (var i = 0; i < array.length; i++) {
-      var index = (array[i] % wordlist.length);
+   for (var i = 0; i < numberOfWords; i++) {
+      var index = Math.floor(randomNumberBetweenZeroAndOne() * wordlist.length)
       generatedPasswordArray.push(wordlist[index]);
   }
 
